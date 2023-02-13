e8f76a47ca098d276b972a5bf46f5951cc333695
monikamr1229 <mahantesh0917@gmail.com>
2023-02-13 17:50:36 +0530

    Sector

---
+++ Sector5.java
@@ -0,0 +1,15 @@
+class Sector5
+{
+	void Sectora()
+	{
+		int t=7;
+		int r=4;
+		double area=(1/2.0)*r*r*t;
+		System.out.println(area);
+
+	}
+	public static void main(String[] args)
+	{
+		new Sector5().Sectora();
+	}
+}
